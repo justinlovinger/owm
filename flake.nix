ff1e685cc54bd064d481f7ffd4cce1f6b5e7a65c
Justin Lovinger <git@justinlovinger.com>
2023-07-29 16:25:29 +0000

    Initialize

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    naersk,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      naersk-lib = pkgs.callPackage naersk {};
+    in {
+      defaultPackage = naersk-lib.buildPackage ./.;
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            cargo
+            rust-analyzer
+            rustc
+            rustfmt
+            rustPackages.clippy
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+    });
+}
