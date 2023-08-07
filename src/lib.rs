779b43946ec58da3f967222d98de3c01fd1ebe54
Justin Lovinger <git@justinlovinger.com>
2023-08-07 11:34:10 +0000

    Customize optimizer
    Optimize longer
    and with more samples.
    Disable mutation
    for faster convergence.
    It has dubious benefits anyway.

--- src/lib.rs
+++ src/lib.rs
@@ -21,13 +21,23 @@ pub fn layout(width: usize, height: usize, count: usize) -> Vec<Window> {
     let decoder = Decoder::new(16, container, count);
     let problem = Problem::new(container, count);
     let mut windows = decoder.decode1(
-        UntilConvergedConfig::default()
-            .argmin(&mut Config::start_default_for(decoder.bits(), |points| {
+        UntilConvergedConfig {
+            threshold: ProbabilityThreshold::new(Probability::new(0.9).unwrap()).unwrap(),
+        }
+        .argmin(
+            &mut Config {
+                num_samples: NumSamples::new(100).unwrap(),
+                adjust_rate: AdjustRate::new(0.1).unwrap(),
+                mutation_chance: MutationChance::new(0.0).unwrap(),
+                mutation_adjust_rate: MutationAdjustRate::new(0.05).unwrap(),
+            }
+            .start(decoder.bits(), |points| {
                 decoder.decode(points).map_axis(Axis(1), |windows| {
                     problem.evaluate(windows.as_slice().unwrap())
                 })
-            }))
-            .view(),
+            }),
+        )
+        .view(),
     );
     remove_gaps(container, windows.view_mut());
     overlap_borders(1, container, windows.view_mut());
