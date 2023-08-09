eda56e5031e475a5cef0bceb9abf24a8aa2433b8
Justin Lovinger <git@justinlovinger.com>
2023-08-09 20:41:13 +0000

    Re-fix River ignoring layout if it took too long to generate

--- src/bin/owm.rs
+++ src/bin/owm.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
-use std::sync::{Arc, Mutex};
+use std::process::Command;
+use std::sync::Mutex;
 use std::thread;
 
 use once_cell::sync::Lazy;
@@ -18,8 +19,6 @@ use wayland_client::{
 use crate::protocol::{
     river_layout_manager_v3::RiverLayoutManagerV3,
     river_layout_v3::{self, RiverLayoutV3},
-    zriver_command_callback_v1::ZriverCommandCallbackV1,
-    zriver_control_v1::ZriverControlV1,
 };
 
 fn main() {
@@ -47,18 +46,12 @@ impl OutputId {
 pub struct LayoutManager {
     // These will be initialized
     // by Wayland events.
-    seat: Option<Arc<WlSeat>>,
     manager: Option<RiverLayoutManagerV3>,
-    control: Option<Arc<Mutex<ZriverControlV1>>>,
 }
 
 impl LayoutManager {
     pub fn new() -> Self {
-        Self {
-            seat: None,
-            manager: None,
-            control: None,
-        }
+        Self { manager: None }
     }
 }
 
@@ -84,14 +77,6 @@ impl Dispatch<WlRegistry, ()> for LayoutManager {
         } = event
         {
             match interface.as_str() {
-                "wl_seat" => {
-                    state.seat = Some(Arc::new(registry.bind::<WlSeat, _, Self>(
-                        name,
-                        version,
-                        qhandle,
-                        (),
-                    )));
-                }
                 "wl_output" => {
                     registry.bind::<WlOutput, _, Self>(name, version, qhandle, ());
                 }
@@ -102,11 +87,6 @@ impl Dispatch<WlRegistry, ()> for LayoutManager {
                         qhandle,
                         (),
                     ));
-                }
-                "zriver_control_v1" => {
-                    state.control = Some(Arc::new(Mutex::new(
-                        registry.bind::<ZriverControlV1, _, Self>(name, version, qhandle, ()),
-                    )));
                 }
                 _ => {}
             }
@@ -148,12 +128,12 @@ impl Dispatch<WlOutput, ()> for LayoutManager {
 
 impl Dispatch<RiverLayoutV3, OutputId> for LayoutManager {
     fn event(
-        state: &mut Self,
+        _: &mut Self,
         proxy: &RiverLayoutV3,
         event: <RiverLayoutV3 as wayland_client::Proxy>::Event,
         _output: &OutputId,
         _: &wayland_client::Connection,
-        qhandle: &wayland_client::QueueHandle<Self>,
+        _: &wayland_client::QueueHandle<Self>,
     ) {
         match event {
             river_layout_v3::Event::LayoutDemand {
@@ -189,27 +169,19 @@ impl Dispatch<RiverLayoutV3, OutputId> for LayoutManager {
                     }
                     None => {
                         if STARTED.lock().unwrap().insert(key) {
-                            let control = Arc::clone(
-                                state
-                                    .control
-                                    .as_ref()
-                                    .expect("River control should be initialized"),
-                            );
-                            let seat = Arc::clone(
-                                state.seat.as_ref().expect("seat should be initialized"),
-                            );
-                            let qhandle = qhandle.clone();
                             thread::spawn(move || {
                                 let layout = layout(usable_width, usable_height, view_count);
                                 CACHE.lock().unwrap().insert(key, layout);
 
                                 // River will send a new layout demand
                                 // if it receives a layout command.
-                                let control = control.lock().unwrap();
-                                control.add_argument("send-layout-cmd".to_owned());
-                                control.add_argument("owm".to_owned());
-                                control.add_argument("retry-layout".to_owned());
-                                control.run_command(&seat, &qhandle, ());
+                                // As of 2023-08-09,
+                                // `wayland-rs` delays sending commands from another thread,
+                                // so we cannot use `river-control` directly,
+                                // see <https://github.com/Smithay/wayland-rs/issues/646>.
+                                let _ = Command::new("riverctl")
+                                    .args(["send-layout-cmd", "owm", "retry-layout"])
+                                    .output();
                             });
                         }
                     }
@@ -235,30 +207,6 @@ impl Dispatch<RiverLayoutManagerV3, ()> for LayoutManager {
     }
 }
 
-impl Dispatch<ZriverControlV1, ()> for LayoutManager {
-    fn event(
-        _: &mut Self,
-        _: &ZriverControlV1,
-        _: <ZriverControlV1 as wayland_client::Proxy>::Event,
-        _: &(),
-        _: &wayland_client::Connection,
-        _: &wayland_client::QueueHandle<Self>,
-    ) {
-    }
-}
-
-impl Dispatch<ZriverCommandCallbackV1, ()> for LayoutManager {
-    fn event(
-        _: &mut Self,
-        _: &ZriverCommandCallbackV1,
-        _: <ZriverCommandCallbackV1 as wayland_client::Proxy>::Event,
-        _: &(),
-        _: &wayland_client::Connection,
-        _: &wayland_client::QueueHandle<Self>,
-    ) {
-    }
-}
-
 mod protocol {
     // See <https://docs.rs/wayland-scanner/latest/wayland_scanner/#example-usage>.
 
@@ -276,14 +224,5 @@ mod protocol {
     }
     use self::__layout_interfaces::*;
 
-    pub mod __control_interfaces {
-        use wayland_client::backend as wayland_backend;
-        use wayland_client::protocol::__interfaces::*;
-
-        wayland_scanner::generate_interfaces!("./protocols/river-control-unstable-v1.xml");
-    }
-    use self::__control_interfaces::*;
-
     wayland_scanner::generate_client_code!("./protocols/river-layout-v3.xml");
-    wayland_scanner::generate_client_code!("./protocols/river-control-unstable-v1.xml");
 }
