60bad5a32cebf48b2bdfd004fbefdaec2316b2ce
Justin Lovinger <git@justinlovinger.com>
2025-05-31 18:55:49 +0000

    Format Nix to RFC style

--- flake.nix
+++ flake.nix
@@ -5,31 +5,37 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    naersk,
-    nixpkgs,
-    utils,
-  }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      naersk-lib = pkgs.callPackage naersk {};
-    in {
-      defaultPackage = naersk-lib.buildPackage {
-        src = ./.;
-        doCheck = true;
-      };
-      devShell = with pkgs;
-        mkShell {
-          buildInputs = [
-            cargo
-            rust-analyzer
-            rustc
-            rustfmt
-            rustPackages.clippy
-          ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          PROPTEST_DISABLE_FAILURE_PERSISTENCE = 1;
+  outputs =
+    {
+      self,
+      naersk,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+          doCheck = true;
         };
-    });
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              rust-analyzer
+              rustc
+              rustfmt
+              rustPackages.clippy
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            PROPTEST_DISABLE_FAILURE_PERSISTENCE = 1;
+          };
+      }
+    );
 }
